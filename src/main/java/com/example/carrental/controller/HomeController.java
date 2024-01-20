9c6b532cc6f6cf5dab2a4c165a5f606e4c8bdb60
Adrian Niedbalski <niedbalskiadrian@gmail.com>
2024-01-20 15:06:48 +0100

    Poczatkowe ustawienia

--- src/main/java/com/example/carrental/controller/HomeController.java
+++ src/main/java/com/example/carrental/controller/HomeController.java
@@ -1,16 +1,10 @@
+package com.example.carrental.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @GetMapping("/")
-
-    public String home() {
-        return "index";
-    }
-
     @GetMapping("/home")
     public String homePage() {
         return "home";
