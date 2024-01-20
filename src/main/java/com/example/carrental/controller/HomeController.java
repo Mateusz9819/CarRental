26d65dfcd181d71cbd0ec25a4b541849519680f8
Adrian Niedbalski <niedbalskiadrian@gmail.com>
2024-01-20 17:02:22 +0100

    Poczatek projektu

--- src/main/java/com/example/carrental/controller/HomeController.java
+++ src/main/java/com/example/carrental/controller/HomeController.java
@@ -5,8 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String homePage() {
-        return "home";
+    @GetMapping("/")
+    public String greeting() {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/employee")
+    public String employee() {
+        return "employee";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/login";
     }
 }
---
+++ src/main/java/com/example/carrental/service/UserDetail.java
@@ -0,0 +1,29 @@
+package com.example.carrental.service;
+
+import com.example.carrental.Entity.User;
+import com.example.carrental.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetail implements UserDetailsService {
+    @Autowired
+    UserRepository userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsernameOrEmail(username, username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not exists by Username or Email"));
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+        return new org.springframework.security.core.userdetails.User(username,user.getPassword(),authorities);
+    }
+}
