17e54f315e5e3afa59302d8c59c0a4bd8eb9f1a0
Mateusz <klinke.mateusz@gmail.com>
2024-01-21 15:02:35 +0100

    ZMIANY DODANE PRZED CHWILA

--- src/main/java/com/example/carrental/controller/HomeController.java
+++ src/main/java/com/example/carrental/controller/HomeController.java
@@ -1,37 +1,28 @@
 package com.example.carrental.controller;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+
+
     @GetMapping("/")
-    public String defaultPage(Authentication authentication) {
-        String username = authentication.name();
+    public String defaultPage() {
         return "index";
     }
+
     @GetMapping("/login")
     public String login() {
         return "login";
     }
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
-    }
-    @GetMapping("/client")
-    public String client() {
-        return "client";
-    }
 
-    @GetMapping("/manager")
-    public String manager() {
-        return "manager";
-    }
-
-    @GetMapping("/employee")
-    public String employee() {
-        return "employee";
+    @GetMapping("/home")
+    public String home() {
+        return "home";
     }
 
     @GetMapping("/logout")
--- src/main/java/com/example/carrental/security/SecurityConfig.java
+++ src/main/java/com/example/carrental/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         form -> form
                                 .loginPage("/login")
                                 .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/")
+                                .defaultSuccessUrl("/home")
                                 .permitAll()
                 ).logout(
                         logout -> logout
